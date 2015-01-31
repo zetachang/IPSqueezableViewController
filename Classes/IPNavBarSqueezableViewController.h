4eb9bfc48c83ef36237102a81eff84694eb88bd8
Yuriy Pitomets <pitometsu@gmail.com>
2015-01-31 09:32:46 +0200

    v0.0.2 Improved
    - Add support for custom title view
    - Fix proper behavior for left and right bar
     button items.

--- Classes/IPNavBarSqueezableViewController.h
+++ Classes/IPNavBarSqueezableViewController.h
@@ -8,9 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface IPNavBarSqueezableViewController : UIViewController
 
-@property (nonatomic, weak) IBOutlet UIScrollView* triggeringScrollView;
-@property (nonatomic, strong) UIBarButtonItem* ip_rightNavBarItem;
+@property (nonatomic, weak) IBOutlet UIScrollView *triggeringScrollView;
+
+@property (nonatomic, strong) UIFont *titleFont;
+
+@property (nonatomic, copy) void (^squeezeCompletion)(void); // nil by default
+@property (nonatomic, copy) void (^expandCompletion)(void); // nil by default
+
+// inheritance
+- (void)processBars NS_REQUIRES_SUPER;
+- (void)squeezeBars NS_REQUIRES_SUPER;
+- (void)expandBars NS_REQUIRES_SUPER;
+
+- (NSString *)squeezedTitle:(NSString *)title; // @"[ %@ ]" by default
 
 @end
